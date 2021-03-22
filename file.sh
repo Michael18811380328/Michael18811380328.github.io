aba49534271b382e6deddfa5ab56af50e1eae19f
Michael An <1822852997@qq.com>
2021-03-22 17:57:12 +0800

    update files

--- file.sh
+++ file.sh
@@ -1,29 +1,75 @@
 #!/bin/bash
 
 # cat ./book/docs/res.md | while read line
-
 # do
 #     echo "- '$line': '$line'"
 #     # 根据实际子目录确定这里的格式
 # done
 
-# cat ./personal/docs/res.md | while read line
 
-# do
-#     echo "- '$line': '$line'"
-#     # 根据实际子目录确定这里的格式
-# done
-
-cat ./book/docs/res.md | while read line
-
+# dir: book
+echo "book start------"
+ls ./book/docs | while read line
 do
-    echo "- '$line': '$line'"
-    # 根据实际子目录确定这里的格式
+  echo "- '$line': '$line'"
 done
+echo "book end------"
+echo -e "\n\n"
+sleep 2
 
-# cat ./backend/docs/res.md | while read line
+# dir personal
+echo "personal start------"
+ls ./personal/docs/old | while read line
+do
+  echo "- '$line': 'old/$line'"
+done
+ls ./personal/docs/ruby | while read line
+do
+  echo "- '$line': 'ruby/$line'"
+done
+ls ./personal/docs | while read line
+do
+  echo "- '$line': '$line'"
+done
+echo "personal end------"
+echo -e "\n\n"
+sleep 2
 
-# do
-#     echo "- '$line': '$line'"
-#     # 根据实际子目录确定这里的格式
-# done
+# dir backend
+echo "backend start------"
+ls ./backend/docs/nodejs | while read line
+do
+  echo "- '$line': 'nodejs/$line'"
+done
+ls ./backend/docs/python | while read line
+do
+  echo "- '$line': 'python/$line'"
+done
+ls ./backend/sql | while read line
+do
+  echo "- '$line': 'sql/$line'"
+done
+ls ./backend | while read line
+do
+  echo "- '$line': '$line'"
+done
+echo "backend end------"
+echo -e "\n\n"
+sleep 2
+
+
+# frontend array and loop
+echo "frontend start------"
+my_array=('' 'about' 'algre' 'git' 'interview' 'javascript' 'netease' 'project' 'react' 'typescript')
+# len=${#my_array[*]} 
+for i in {0..9}
+do
+  str="${my_array[i]}"
+  ls "./frontend/docs/${str}" | while read line
+  do
+    echo "- '$line': '${str}/$line'"
+  done
+  echo -e "\n"
+done
+echo "frontend end------"
+echo -e "\n\n"
