4ceee81874918d8b451da5bb2fec4783838c5f13
Michael An <1822852997@qq.com>
2021-06-08 17:28:55 +0800

    add

---
+++ book/site/ebook-netease-senior-frontend/21/demo/app.vue
@@ -0,0 +1,14 @@
+<template>
+  <div id="app">
+    <img src='./logo.png'/>
+    <router-view/>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'App'
+}
+</script>
+
+<style></style>
---
+++ frontend/site/react/react-all/中文文档全部/demo/test.sh
@@ -0,0 +1,40 @@
+#!/bin/bash
+# for a in {1..5}
+# do
+#         mkdir /datas/aaa$a
+#         cd /datas/aaa$a
+#         for b in {1..5}
+#         do
+#                 mkdir bbb$b
+#         done
+# done
+
+# for a in {1..5}
+# do
+#         rm -rf ./bbb$a
+# done
+
+
+# mkdir 05
+# cd 05
+# for a in {1..8}
+# do
+#         touch 05-$a-todo.js
+# done
+# cd ../
+
+# mkdir 04
+# cd 04
+# for b in {1..10}
+# do
+#         touch 04-$b-todo.js
+# done
+# cd ../
+
+# mkdir 03
+# cd 03
+# for c in {8..22}
+# do
+#         touch 03-$c-todo.js
+# done
+# cd ../
