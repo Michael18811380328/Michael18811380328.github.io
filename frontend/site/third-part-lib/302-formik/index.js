0a3f0c97eb2b6a0ed98e8985388ba27e5edbad61
Michael An <2331806369@qq.com>
2022-03-20 10:52:19 +0800

    update to 2.0.4

---
+++ frontend/site/third-part-lib/302-formik/index.js
@@ -0,0 +1,38 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+// 引入表单组件
+import { Formik, Field, Form } from 'formik';
+
+const Basic = () => (
+  <div>
+    <h1>Sign Up</h1>
+    <Formik
+      // 设置每一个项的初始值
+      initialValues={{
+        firstName: '',
+        lastName: '',
+        email: '',
+      }}
+      // 设置回调函数（点击提交按钮后）
+      onSubmit={async (values) => {
+        await new Promise((r) => setTimeout(r, 500));
+        alert(JSON.stringify(values, null, 2));
+      }}
+    >
+      <Form>
+        <label htmlFor="firstName">First Name</label>
+        <Field id="firstName" name="firstName" placeholder="Jane" />
+
+        <label htmlFor="lastName">Last Name</label>
+        <Field id="lastName" name="lastName" placeholder="Doe" />
+
+        <label htmlFor="email">Email</label>
+        <Field id="email" name="email" placeholder="jane@acme.com" type="email"/>
+        
+        <button type="submit">Submit</button>
+      </Form>
+    </Formik>
+  </div>
+);
+
+ReactDOM.render(<Basic />, document.getElementById('root'));
